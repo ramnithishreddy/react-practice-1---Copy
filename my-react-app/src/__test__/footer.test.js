d8abf8b82ee0b7ca8c66fd63da2b87091db7bdf7
Ram Nithish <69964760+ramnithishreddy@users.noreply.github.com>
2024-10-11 08:40:51 +0000

    test coverage

---
+++ my-react-app/src/__test__/footer.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import Footer from "../amazon/footer";
+
+describe("Footer", () => {
+  it("should render", () => {
+    render(<Footer />);
+  });
+});
