b417e4f15e3e5b4044c56e98249e6bb1ed65788e
Ram Nithish Reddy <69964760+ramnithishreddy@users.noreply.github.com>
2026-03-10 09:24:54 +0000

    Fix Redux context error by wrapping App with Provider in index.js
    - Added Redux Provider import from react-redux
    - Imported store from redux/store
    - Wrapped CartProvider and App with Redux Provider at root level
    - Fixes: Cannot destructure property 'store' of 'reduxContext' as it is null
    - Nav component can now properly access Redux state via useSelector

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import CartProvider from "./amazon/cartProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <Provider store={store}>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </Provider>
   </React.StrictMode>
 );
 
